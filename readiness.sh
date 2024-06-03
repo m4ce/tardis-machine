bdf5e389d6e0e06e865b774de932d23f7cafdbd4
Matteo Cerutti <matteo.cerutti@numeus.xyz>
2024-06-03 13:43:58 +0100

    fixing

--- readiness.sh
+++ readiness.sh
@@ -12,7 +12,7 @@ if [ $disk_used_pct -gt $DISK_SIZE_THRESHOLD_PCT ]; then
   exit 1
 fi
 
-load_avg1=`cat /proc/loadavg | cut -d' '-f 1`
+load_avg1=`cat /proc/loadavg | cut -d' ' -f 1`
 if [ $load_avg -gt $AVAILABLE_CPUS ]; then
   exit 1
 fi
